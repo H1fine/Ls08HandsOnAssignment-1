6e9c7733af4b9ecd1f261fce253103d730b9e252
H1fine <111656813+H1fine@users.noreply.github.com>
2023-04-30 22:03:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ls08HandsOnAssignment-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-eej1r7)
---
+++ src/store/action-creators.js
@@ -0,0 +1,10 @@
+
+import { TYPE_DECREMENT, TYPE_INCREMENT } from "./action-types";
+
+export const decrement = () => ({
+    type: TYPE_DECREMENT,
+});
+
+export const increment = () => ({
+    type: TYPE_INCREMENT,
+});
